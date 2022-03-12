58edc04eeb12511e919fd6daceb8163ab7fc2ad8
Ethan Collopy <19819315+scienceMiner@users.noreply.github.com>
2022-03-12 18:07:14 +0000

    Update pages

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,10 +5,10 @@
     </div>
     <div class="section details">
       <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers we would like to suggest people make a donation to one of the following charities</p>
+      <p>Instead of flowers we would like to suggest people make a donation to:</p>
       <h2>The Stroke Association</h2>
-      <p><em>The Stroke Association</em>'s goal of helping stroke survivors and their families rebuild their lives in ways that no one else can. Their specialist support helps them find the hope they need to move forward with their recovery.</p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Make a Wish</a>
+      <p><em>The Stroke Association</em>'s goal is to help stroke survivors and their families rebuild their lives in ways that no one else can. Their specialist support helps them find the hope they need to move forward with their recovery.</p>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to The Stroke Association</a>
     </div>
   </div>
 </template>
--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,14 +5,12 @@
     </div>
     <div class="section details">
       <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
+      <p>As attendance is restricted due to COVID-19 we hope that you will join us
+      virtually. The services will be held on <em>TBA</em>,
+      at Crematorium (address TBA).</p>
       <h2>Crematorium</h2>
-      <p>The service is being held at Crematorium address</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
+      <p>The service is being held at Crematorium TBA </p>
+      <a class="button" target="_blank" rel=”noopener” href="">TBA - 12:00pm</a>
       <ul class="fa-ul">
         <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
         <ListItem>Password: <span class="highlight">password123</span> <CopyButton text="password123"></CopyButton></ListItem>
