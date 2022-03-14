5aaf84573eb6cccbfe0546aeb452523a59e87c3e
Ethan Collopy <19819315+scienceMiner@users.noreply.github.com>
2022-03-14 22:22:32 +0000

    Add charity link

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -8,7 +8,7 @@
       <p>Instead of flowers we would like to suggest people make a donation to:</p>
       <h2>The Stroke Association</h2>
       <p><em>The Stroke Association</em>'s goal is to help stroke survivors and their families rebuild their lives in ways that no one else can. Their specialist support helps them find the hope they need to move forward with their recovery.</p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to The Stroke Association</a>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/georgecollopymemorial/">Donate to The Stroke Association</a>
     </div>
   </div>
 </template>
