31f01ca5077eae99cd3317879214764545991912
Ethan Collopy <19819315+scienceMiner@users.noreply.github.com>
2022-03-16 18:06:55 +0000

    add embeds

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -20,8 +20,11 @@
       <div class="col span1_3">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.6879653476803!2d-0.18319358422688184!3d51.592281979649165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761a0443c878c3%3A0xff9f99aea8037287!2sE+End+Rd%2C+London+N2+0RZ!5e0!3m2!1sen!2suk!4v1561132830461!5m2!1sen!2suk" width="100%" height="300px" allowfullscreen></iframe>
 
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.6878967405814!2d-0.18650656361839785!3d51.59228323694859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761a04d331eac9%3A0xe13741abd894cf3d!2sSt%20Marylebone%20Crematorium!5e0!3m2!1sen!2suk!4v1647453734439!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
           <p><a href="https://goo.gl/maps/dMV56B58aAtXNnpA8" target="_blank">Click for Full directions &gt;&gt;</a></p>
+          <p><a href="https://goo.gl/maps/furUZGmz7QRTJX9H8" target="_blank">Click for Full directions &gt;&gt;</a></p>
       </div>
+      <a class="button" target="_blank" rel=”noopener” href="https://goo.gl/maps/furUZGmz7QRTJX9H8">Click for Full Directions </a>
 
     </div>
   </div>
