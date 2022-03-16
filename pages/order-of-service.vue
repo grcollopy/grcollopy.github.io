6c13309243e11d4e659d8aea7ecf24df3a33cd0b
Ethan Collopy <19819315+scienceMiner@users.noreply.github.com>
2022-03-16 17:45:53 +0000

    update fd page

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,12 +5,12 @@
     </div>
     <div class="section details">
       <h1 class="title">Funeral Details</h1>
-      <p><em>Date Forthcoming</em> We do not have details confirmed at the moment.</p>
-      <p class="highlight">All Details TBA</p>
-      <h2>Crematorium <span>Time: TBA</span></h2>
+      <p> <em>12th April 2022</em> at 11am at St. Marylebone Crematorium </p>
+      <p class="highlight"><a href="https://www.thelondoncremation.co.uk/south-east-crematoriums/st-marylebone/"> St. Marylebone Crematorium</p>
+      <h2>Notes:</h2>
       <ul class="fa-ul">
-        <ListItem>Service starts at TBA at Crematorium address</ListItem>
-
+        <ListItem>Service starts at 11:00 AM at St. Marylebone Crematorium</ListItem>
+        <ListItem>Address Details: St Marylebone Crematorium, East End Road, Finchley, London, N2 0RZ</ListItem>
       </ul>
     </div>
   </div>
