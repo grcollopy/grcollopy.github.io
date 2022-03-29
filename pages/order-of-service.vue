1e81b404af007f2112cf2abab00bfea2cab89a94
Ethan Collopy <19819315+scienceMiner@users.noreply.github.com>
2022-03-29 20:47:09 +0100

    update for OWL

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -9,15 +9,17 @@
 
       <a class="button" target="_blank" rel=”noopener” href="https://www.thelondoncremation.co.uk/south-east-crematoriums/st-marylebone/">Visit St. Marylebone Crematorium Site </a>
 
-      <p> Please join us after the service at TBA </p>
+      <p> Afterwards, please join us at The Old White Lion, East Finchley </p>
 
-      <a class="button" target="_blank" rel=”noopener” href=""> Visit TBA </a>
+      <a class="button" target="_blank" rel=”noopener” href="https://www.theoldwhitelionfinchley.co.uk/"> Visit The Old White Lion </a>
 
       <h2>Notes:</h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 11:00 AM at St. Marylebone Crematorium</ListItem>
         <ListItem>Address Details: St Marylebone Crematorium, East End Road, Finchley, London, N2 0RZ</ListItem>
-      </ul>
+        <ListItem>Address for The Old White Lion: 121 Great N Rd, London, N2 0NW </ListItem>
+        <ListItem>Parking at The Old White Lion: there is space for 5-6 cars, also parking is available at East Finchley Tube Station, 5 minutes walk away </ListItem>
+</ul>
 
       <div class="col span1_3">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.6878967405814!2d-0.18650656361839785!3d51.59228323694859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761a04d331eac9%3A0xe13741abd894cf3d!2sSt%20Marylebone%20Crematorium!5e0!3m2!1sen!2suk!4v1647453734439!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
